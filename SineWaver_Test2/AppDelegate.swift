ce002008f4eb8b9e7b9327fe2c93747f2dd49d9b
george <george@georges-MacBook-Pro-2.local>
2015-02-10 17:24:46 +1000

    a starting point ...the objective here is to draw a line that has it's start point y value and ending point y value controlled by 2 sliders

--- SineWaver_Test2/AppDelegate.swift
+++ SineWaver_Test2/AppDelegate.swift
@@ -13,11 +13,56 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
+    @IBOutlet weak var DrawingView1: NSView!
 
+    @IBOutlet weak var leftSlider: NSSlider!
+    
+    @IBAction func leftSlidersender(sender: NSSlider) {
+        
+        println("test")
+        var x: Double = sender.doubleValue
+        println("\(Int(x))")
+    }
+   
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+      
+        
+     DrawingView1.lockFocus()
+        
+        var path = NSBezierPath()
+        
+        var startY = Int(0)
+        var endY = Int(0)
+        var startPoint = NSPoint(x:0, y:(startY))
+        var endingPoint = NSPoint(x:500, y:(endY))
+        
+//        path.moveToPoint(NSPoint(x: 0, y: 0))
+//        
+//        path.lineToPoint(NSPoint(x: 500, y:0))
+        
+        path.moveToPoint(startPoint)
+        
+        path.lineToPoint(endingPoint)
+        
+        path.stroke()
+        
+       DrawingView1.unlockFocus()
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 
+    
+    
+    
+    
+    
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
